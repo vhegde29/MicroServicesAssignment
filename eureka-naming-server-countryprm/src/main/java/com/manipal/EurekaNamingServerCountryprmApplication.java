1525ad02f82c68b41984bc4d16de8cdfc73a6812
vineeth hegde <vinnyhegde1997@gmail.com>
2020-07-26 23:44:00 +0530

    eureka naming

---
+++ eureka-naming-server-countryprm/src/main/java/com/manipal/EurekaNamingServerCountryprmApplication.java
@@ -0,0 +1,15 @@
+package com.manipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaNamingServerCountryprmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNamingServerCountryprmApplication.class, args);
+	}
+
+}
